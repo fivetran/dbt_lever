1360c4805c469c3c9e397c1fe7ccd51805ede386
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-01-27 14:28:40 -0800

    fill valid_ending_at with archived_at or current timestamp

--- models/lever__opportunity_stage_history.sql
+++ models/lever__opportunity_stage_history.sql
@@ -25,19 +25,52 @@ opp_stage_history as (
         updater_user_id,
         to_stage_index as stage_index_in_pipeline,
         lead(updated_at) over (partition by opportunity_id order by updated_at asc) as valid_ending_at
+
     from {{ var('opportunity_stage_history') }}
 ),
 
-time_in_stages as (
+-- joining first to get opportunity.archived_at for the valid_ending_at column
+join_opportunity_stage_history as (
 
-    select
-        *,
-        {{ dbt_utils.datediff('valid_from', 'valid_ending_at', 'day') }} as days_in_stage
-        
+    select 
+        opp_stage_history.opportunity_id,
+        opp_stage_history.valid_from,
+        opp_stage_history.stage_id,
+        opp_stage_history.updater_user_id,
+        opp_stage_history.stage_index_in_pipeline,
+
+        coalesce(opp_stage_history.valid_ending_at, opportunity.archived_at, {{ dbt_utils.current_timestamp() }}) as valid_ending_at,
+
+        stage.stage_name as stage,
+        lever_user.full_name as updater_user_name,
+        opportunity.contact_name,
+        opportunity.archive_reason, -- if archived later
+        opportunity.job_title,
+        opportunity.job_department,
+        opportunity.job_location,
+        opportunity.job_team,
+        opportunity.application_type,
+        opportunity.sources as application_sources,
+        opportunity.hiring_manager_name,
+        opportunity.opportunity_owner_name
+
     from opp_stage_history
 
+    join stage on opp_stage_history.stage_id = stage.stage_id
+    left join lever_user on lever_user.user_id = opp_stage_history.updater_user_id 
+    join opportunity on opportunity.opportunity_id = opp_stage_history.opportunity_id
 ),
 
+final_time_in_stages as (
+
+    select
+       *,
+        {{ dbt_utils.datediff('valid_from', 'valid_ending_at', 'day') }} as days_in_stage
+        
+    from join_opportunity_stage_history
+
+)
+{# 
 final as (
 
     select 
@@ -59,6 +92,6 @@ final as (
     join stage on time_in_stages.stage_id = stage.stage_id
     left join lever_user on lever_user.user_id = time_in_stages.updater_user_id 
     join opportunity on opportunity.opportunity_id = time_in_stages.opportunity_id
-)
+) #}
 
-select * from final
+select * from final_time_in_stages
